6638e969a228e1b90f9b1c5199e13dc69c7f4cf6
Ronit Suman <ronitsuman59@gmail.com>
2024-07-26 14:56:16 +0530

    m

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Header from "./components/Header"
+import Body from "./components/Body"
+import Footer from"./components/Footer"
+
+function App() {
+  
+
+  return (
+    <>
+    
+    <div  className="min-h-screen ,px-6,py-3 ">
+      <Header/>
+      <Body/>
+      <Footer/>
+      
+      </div>
+    </>
+  )
+}
+
+export default App
